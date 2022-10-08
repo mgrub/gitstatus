392f0a3e033584b3a6628d65eeaeb3bed9770f8f
Maximilian Gruber <m.gruber@mailbox.org>
2022-10-08 21:03:26 +0200

    clean up after compilation

--- compile_all.sh
+++ compile_all.sh
@@ -7,3 +7,7 @@ latex gitstatus.ins
 pdflatex gitstatus.dtx
 makeindex -s gind.ist -o gitstatus.ind gitstatus.idx
 pdflatex gitstatus.dtx
+
+# clean up
+latexmk -c gitstatus.dtx
+rm gitstatus.log gitstatus.sty gitstatus.xwm gitstatus.glo gitstatus.dvi
