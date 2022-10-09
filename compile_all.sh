d43553b1b4696f3730304ef371e1632ad74f78e0
Maximilian Gruber <m.gruber@mailbox.org>
2022-10-09 09:37:31 +0200

    add generation of zip archive to compile script

--- compile_all.sh
+++ compile_all.sh
@@ -11,3 +11,11 @@ pdflatex gitstatus.dtx
 # clean up
 latexmk -c gitstatus.dtx
 rm gitstatus.log gitstatus.sty gitstatus.xwm gitstatus.glo gitstatus.dvi
+
+# prepare CTAN zip
+rm gitstatus.zip
+mkdir gitstatus
+cp example.tex gitstatus.dtx gitstatus.ins LICENSE README.md gitstatus/
+zip gitstatus.zip gitstatus/*
+rm gitstatus/*
+rmdir gitstatus
